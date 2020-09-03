8cfb336b6004ec963d7ac4ec7ec5b07326dd2b99
hong_yu.g <hong_yu.g@foxmail.com>
2020-09-03 19:55:09 +0800

    update

--- order-service/src/main/java/com/hong/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/hong/orderservice/OrderServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients(basePackages = "com.hong")
 @EnableHystrix
 @EnableHystrixDashboard
+@RibbonClient("user-service")//启动ribbon
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/hong/orderservice/config/RestConfig.java
+++ order-service/src/main/java/com/hong/orderservice/config/RestConfig.java
@@ -1,5 +1,7 @@
 package com.hong.orderservice.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,9 +29,9 @@ public class RestConfig {
      * RandomRule 随机
      * AvailabilityFilteringRule
      */
-//    @Bean
-//    public IRule getIRule() {
-//        return new RoundRobinRule();
-//    }
+    @Bean
+    public IRule getIRule() {
+        return new RoundRobinRule();
+    }
 
 }
